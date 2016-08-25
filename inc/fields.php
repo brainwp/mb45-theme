3a22af5838cf9d37898f778f5bdcb593445081f4
Isaque Melo <isaquegbmelo2@gmail.com>
2016-08-25 16:54:27 -0300

    faqs template. fields correction #8 #16

--- inc/fields.php
+++ inc/fields.php
@@ -4,3 +4,44 @@
  * Odin ACF Fields
  *
 */
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_about-us',
+		'title' => 'About Us',
+		'fields' => array (
+			array (
+				'key' => 'field_57be2eaf4c1b0',
+				'label' => 'Sub Title',
+				'name' => 'sub_title',
+				'type' => 'text',
+				'instructions' => 'Sub Title',
+				'required' => 1,
+				'default_value' => 'Founder',
+				'placeholder' => 'Sub Title',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => 15,
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page_parent',
+					'operator' => '==',
+					'value' => '17',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 1,
+	));
+}
