be66c170cce2651e4f69689443696c98317a7c4c
matheusgimenez <contato@matheusgimenez.com.br>
2016-08-26 09:22:42 -0300

    colocando a home como um page template

--- page-home.php
+++ page-home.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme and one of the
- * two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
+ * Template Name: Home page
  *
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
@@ -14,7 +9,6 @@
  */
 
 get_header(); ?>
-
 <div id="fullpage">
 
 	<div class="section">
