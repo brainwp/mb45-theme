00f4e4b5ee2b66e3db5c160d6fd5c18fc0d0e250
Isaque Melo <isaquegbmelo2@gmail.com>
2016-08-31 19:18:38 -0300

    page hair #5 full

---
+++ page-hair.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template Name: Hair Page
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header(); ?>
+
+<div id="fullpage-hair">
+
+	<div class="section">
+		<div class="container-fluid">
+			<div class="hold">
+				<div class="hair-name">
+					<span>DOWN</span>
+					<span>1</span>
+				</div>
+				<div class="row">
+					<div class="col-md-6 image1"></div>
+					<div class="col-md-6 image2"></div>
+				</div>
+
+				<div class="row hidden-xs">
+					<div class="col-md-6 image3"></div>
+					<div class="col-md-6 image4"></div>
+				</div>
+
+
+			</div>
+		</div>
+		<!-- //MOBILE -->
+		<div class="col-xs-12 hair-nameM hidden-lg-down">
+				<span>DOWN</span>
+				<span>1</span>
+		</div>
+
+	</div>
+
+	<div class="section">
+		<div class="container-fluid">
+			<div class="hold">
+				<div class="hair-name">
+					<span>DOWN</span>
+					<span>2</span>
+				</div>
+				<div class="row">
+					<div class="col-md-6 image1"></div>
+					<div class="col-md-6 image2"></div>
+				</div>
+
+				<div class="row hidden-xs">
+					<div class="col-md-6 image3"></div>
+					<div class="col-md-6 image4"></div>
+				</div>
+
+
+			</div>
+		</div>
+		<!-- //MOBILE -->
+		<div class="col-xs-12 hair-nameM hidden-lg-down">
+				<span>DOWN</span>
+				<span>2</span>
+		</div>
+
+	</div>
+
+</div>
+
+<?php
+get_footer();
