d2200c65bcf580d87beae06a674957bbdc344eca
Isaque Melo <isaquegbmelo2@gmail.com>
2016-10-12 15:34:47 -0300

    footer back

--- footer.php
+++ footer.php
@@ -12,76 +12,19 @@
 		<footer id="footer" role="contentinfo">
 				<div class="container-fluid">
 					<div class="col-md-12">
+						<?php if ( is_active_sidebar( 'footer-sidebar' ) ) : ?>
+							<?php dynamic_sidebar( 'footer-sidebar' );?>
+						<?php endif;?>
+						<?php if ( is_active_sidebar( 'footer-sidebar-2' ) ) : ?>
+							<?php dynamic_sidebar( 'footer-sidebar-2' );?>
+						<?php endif;?>
+						<?php if ( is_active_sidebar( 'footer-sidebar-3' ) ) : ?>
+							<?php dynamic_sidebar( 'footer-sidebar-3' );?>
+						<?php endif;?>
+						<?php if ( is_active_sidebar( 'footer-sidebar-4' ) ) : ?>
+							<?php dynamic_sidebar( 'footer-sidebar-4' );?>
+						<?php endif;?>
 
-						<div class="footerMenu col-md-3">
-							<span class="footerMenuTitle">
-								OUR COMPANY
-							</span>
-							<ul>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-							</ul>
-						</div>
-
-						<div class="footerMenu col-md-3">
-							<span class="footerMenuTitle">
-								FOLLOW US
-							</span>
-							<ul>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-
-							</ul>
-						</div>
-
-						<div class="footerMenu col-md-3">
-							<span class="footerMenuTitle">
-								OUR COMPANY
-							</span>
-							<ul>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-							</ul>
-						</div>
-
-						<div class="footerMenu col-md-3">
-							<span class="footerMenuTitle">
-								OUR COMPANY
-							</span>
-							<ul>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-								<li>
-									<a href="#">ABOUT</a>
-								</li>
-							</ul>
-						</div>
 
 
 					</div>
