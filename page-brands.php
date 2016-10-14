405770d5c19e8823d741314587b7b314930efd2d
Isaque Melo <isaquegbmelo2@gmail.com>
2016-10-13 22:23:45 -0300

    #7

---
+++ page-brands.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: BRANDS & UPDOS page
+ *
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+get_header(); ?>
+<div id="fullpage-nails">
+
+	<div class="section">
+		<div class="container-fluid">
+			<div class="col-md-offset-1 col-md-5">
+				<div class="theTitle">
+					manicure + blowout 45 minutes
+				</div>
+				<div class="info col-md-8 hidden-xs">
+					Chic Hair + Chic Nails in 45 Minutes
+Mani + Blowout = $65
+				</div>
+				<
+			</div>
+		</div>
+	</div>
+	<div class="section two">
+		<div class="container-fluid">
+			<div class="col-md-offset-1 col-md-5">
+				<div class="theTitle">
+					manicure + blowout 45 minutes
+				</div>
+				<div class="info col-md-8 hidden-xs">
+					Chic Hair + Chic Nails in 45 Minutes
+Mani + Blowout = $65
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+
+</div>
+<?php
+get_footer();
