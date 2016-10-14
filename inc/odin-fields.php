3ed22bc1aae20f26695323f457b84eb45050a4d5
matheusgimenez <contato@matheusgimenez.com.br>
2016-10-14 16:03:55 -0300

    change metabox title

--- inc/odin-fields.php
+++ inc/odin-fields.php
@@ -6,7 +6,7 @@
 */
 $page_galeria = new Odin_Metabox(
     'gallery_hair', // Slug/ID do Metabox (obrigatório)
-    'Página Hair', // Nome do Metabox  (obrigatório)
+    'Page Hair', // Nome do Metabox  (obrigatório)
     'page', // Slug do Post Type, sendo possível enviar apenas um valor ou um array com vários (opcional)
     'normal', // Contexto (opções: normal, advanced, ou side) (opcional)
     'default' // Prioridade (opções: high, core, default ou low) (opcional)
