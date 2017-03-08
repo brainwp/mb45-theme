7500b9c2fb6542e07c47ac4d618869eb75a3fed5
Isaque Melo <isaquegbmelo2@gmail.com>
2017-03-08 19:00:52 -0300

    page not found

--- 404.php
+++ 404.php
@@ -7,22 +7,24 @@
  */
 
 get_header(); ?>
+	<div class="container">
+		<div class="row">
+			<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
 
-	<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
+					<header class="page-header">
 
-			<header class="page-header">
+						<h1 class="page-title"><?php _e( 'Not Found.', 'odin' ); ?></h1>
 
-				<h1 class="page-title"><?php _e( 'Not Found', 'odin' ); ?></h1>
+					</header>
 
-			</header>
+					<div class="page-content">
+						<p><?php _e( 'It looks like nothing was found at this location.', 'odin' ); ?></p>
 
-			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'odin' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .page-content -->
 
-				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
-
-	</main><!-- #main -->
-
+			</main><!-- #main -->
+		</div>
+	</div>
 <?php
 get_footer();
