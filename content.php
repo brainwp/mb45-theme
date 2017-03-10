7bb5d6bf167d24e75e860663704423f42f769ac5
matheusgimenez <contato@matheusgimenez.com.br>
2017-03-10 18:20:29 -0300

    issue #22 - single de posts (blog)

--- content.php
+++ content.php
@@ -9,16 +9,29 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h3 class="entry-title">
-			<a href="<?php the_permalink();?>">
-				<?php the_title();?>
-			</a>
-		</h3><!-- .entry-title -->
+		<?php if ( is_singular() ) : ?>
+			<h1 class="entry-title">
+				<a href="<?php the_permalink();?>">
+					<?php the_title();?>
+				</a>
+			</h1><!-- .entry-title -->
+		<?php else : ?>
+			<h3 class="entry-title">
+				<a href="<?php the_permalink();?>">
+					<?php the_title();?>
+				</a>
+			</h3><!-- .entry-title -->
+		<?php endif;?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content line col-md-6 col-xs-10">
+	<div class="entry-content line col-md-8 col-xs-10">
 		<?php
 			the_content();
 		?>
 	</div><!-- .entry-content -->
+	<?php if( is_singular() && comments_open() ) : ?>
+		<div class="entry-content line col-md-8 col-xs-10">
+			<?php comments_template();?>
+		</div><!-- .entry-content line col-md-6 col-xs-10 -->
+	<?php endif;?>
 </article><!-- #post-## -->
--- single.php
+++ single.php
@@ -1,35 +1,29 @@
 <?php
 /**
- * The Template for displaying all single posts.
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
  *
  * @package Odin
  * @since 2.2.0
  */
-
 get_header(); ?>
+	<!-- <?php echo odin_classes_page_full(); ?> -->
+	<main id="content" class="" tabindex="-1" role="main">
+	<?php
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
 
-	<div id="primary" class="<?php echo odin_classes_page_sidebar(); ?>">
-		<main id="main-content" class="site-main" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile;
-			?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			// Include the page content template.
+			get_template_part( 'content' );
+		endwhile;
+	?>
+	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
