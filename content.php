f03b09917d8c006b1d44c86560ff9624f863c983
matheusgimenez <contato@matheusgimenez.com.br>
2017-03-10 17:35:41 -0300

    issue #22 - archive do blog

--- content.php
+++ content.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * The default template for displaying content.
- *
- * Used for both single and index/archive/author/catagory/search/tag.
+ * The template used for displaying page content.
  *
  * @package Odin
  * @since 2.2.0
@@ -11,46 +9,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
-		?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php odin_posted_on(); ?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<h3 class="entry-title">
+			<a href="<?php the_permalink();?>">
+				<?php the_title();?>
+			</a>
+		</h3><!-- .entry-title -->
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-	<?php else : ?>
-		<div class="entry-content">
-			<?php
-				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'odin' ) );
-				wp_link_pages( array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'odin' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-				) );
-			?>
-		</div><!-- .entry-content -->
-	<?php endif; ?>
-
-	<footer class="entry-meta">
-		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) ) : ?>
-			<span class="cat-links"><?php echo __( 'Posted in:', 'odin' ) . ' ' . get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'odin' ) ); ?></span>
-		<?php endif; ?>
-		<?php the_tags( '<span class="tag-links">' . __( 'Tagged as:', 'odin' ) . ' ', ', ', '</span>' ); ?>
-		<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'odin' ), __( '1 Comment', 'odin' ), __( '% Comments', 'odin' ) ); ?></span>
-		<?php endif; ?>
-	</footer>
+	<div class="entry-content line col-md-6 col-xs-10">
+		<?php
+			the_content();
+		?>
+	</div><!-- .entry-content -->
 </article><!-- #post-## -->
